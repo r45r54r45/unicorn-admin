f1b63edef5bf26b677ac2af6137f6dee67dba78d
r45r54r45 <r45r54r45>
2016-08-18 09:05:53 +0000

    ff

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ router.get('/', function(req, res, next) {
     var exec = require('child_process').exec;
     function puts(error, stdout, stderr) { sys.puts(stdout) }
     exec("git pull origin master", puts);
-    res.render('index');
   }else{
     res.render('index');
   }
